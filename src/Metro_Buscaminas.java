93e2d7e1dd9c6cd0e5a0995f9723e2d5f42213f4
Cesar Atencio <167229343+cesargatencio@users.noreply.github.com>
2025-03-01 19:04:15 -0400

    lr

--- src/Metro_Buscaminas.java
+++ src/Metro_Buscaminas.java
@@ -1,3 +1,6 @@
+
+import MetroInterfaz.Start;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -16,6 +19,8 @@ public class Metro_Buscaminas {
         // Creación de un tablero 3x3 de prueba
         Tablero t = new Tablero(3, 3);
         System.out.println("Tablero creado con éxito.");
+        Start S = new Start();
+        
     }
     
 }
